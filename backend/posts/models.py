65a54b409ffad515c776aaf0791caa33aace0e18
Fernando Popoca <popoca.ortiz@gmail.com>
2023-11-29 19:00:02 -0600

    abstract base model for comments and post, model for reactions

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -8,7 +8,7 @@ from users.models import get_user_model, UserType
 User = get_user_model()
 
 
-class Post(models.Model):
+class Base(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author: UserType = models.ForeignKey(User, on_delete=models.CASCADE)  # type: ignore
@@ -16,18 +16,21 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        abstract = True
+
+
+class Post(Base):
+    
+    class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return f"{self.author.first_name}: {self.content[:100]}"
 
 
-class Comment(models.Model):
+class Comment(Base):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    author: UserType = models.ForeignKey(User, on_delete=models.CASCADE)  # type: ignore
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
 
     def __str__(self):
         return f"{self.author.first_name} {self.content}"
