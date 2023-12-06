95eab2ca6212c05a9fe1f9b2e144cd065ab12419
Fernando Popoca <popoca.ortiz@gmail.com>
2023-12-05 19:09:43 -0600

    fetch and render posts

--- frontend/src/features/posts/Posts.tsx
+++ frontend/src/features/posts/Posts.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const apiHost = process.env.API_HOST;
 
 export function Posts() {
-    return <h1>Posts</h1>;
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+
+        const getPosts = async () => {
+            try {
+
+                const response = await fetch(`${apiHost}/posts`);
+            
+                if (!response.ok) {
+                    console.error('Failed to fetch posts');
+                    return
+                }
+    
+                const postsData = await response.json();
+    
+                setPosts(postsData);   
+
+            } catch (error) {
+                console.log('Error fetching posts: ', error)
+            }
+        }
+
+        getPosts();
+
+    }, [])
+
+    return (<>
+        <h1>Posts</h1>
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>
+                    {post.content}
+                </li>
+            ))}
+        </ul>
+    </>);
 }
