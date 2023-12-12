654f62e9f4b91f81d3f01e5b63579e9931b6a6bb
Fernando Popoca <popoca.ortiz@gmail.com>
2023-12-12 16:12:22 -0600

    react router setup and routes for posts, signup, login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import Posts from './features/posts/Posts';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
 import './App.css';
+
+import Posts from './features/posts/Posts';
 import Header from './components/Header';
+import UsersSignUp from './features/users/register/Register';
+import Login from './features/users/login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <>
+      <Header />
+      <Outlet />
+    </>,
+    children: [
+      {
+        path: 'posts',
+        element: <Posts />
+      },
+      {
+        path: 'sign-up',
+        element: <UsersSignUp />
+      }
+      ,
+      {
+        path: 'login',
+        element: <Login />
+      }
+    ]
+  }
+]);
 
 function App() {
-  return (<>
-    <Header />
-    <Posts />
-  </>);
+  return (<RouterProvider router={router} />);
 }
 
 export default App;
