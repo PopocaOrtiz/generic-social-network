22154f8d801ce164725e5a8153ce0cf0aa94ae18
Fernando Popoca <popoca.ortiz@gmail.com>
2023-12-18 18:49:37 -0600

    fixture for create posts

--- backend/posts/tests/conftest.py
+++ backend/posts/tests/conftest.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Optional
+
+import pytest
 
 from posts import models
+from users.models import UserType
 
 
-CreatePostFixture = Callable[[], models.Post]
+CreatePostFixture = Callable[[Optional[UserType]], models.Post]
+
+
+@pytest.fixture
+def create_post(create_user) -> CreatePostFixture:
+    def _create_post(user = None):
+
+        if user is None:
+            user = create_user()
+
+        return models.Post.objects.create(author=user, content='post content')
+
+    return _create_post
