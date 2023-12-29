cafec93a0e6948331c986f185ddf1d3544f4ee65
Fernando Popoca <popoca.ortiz@gmail.com>
2023-12-28 19:21:54 -0600

    fetch posts now accepts query

--- frontend/src/features/posts/api.ts
+++ frontend/src/features/posts/api.ts
@@ -1,7 +1,13 @@
 import { fetchData, createData } from '../../app/api';
 
-export function fetchPosts() {
-    return fetchData(`posts/`);
+export function fetchPosts(query?: string) {
+    let endpoint = `posts/`
+
+    if (query) {
+        endpoint += `?search=${query}`
+    }
+
+    return fetchData(endpoint);
 }
 
 export function createPost(post: {content: string, image_file: File | null}) {
